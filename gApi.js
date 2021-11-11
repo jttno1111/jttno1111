d1738bda8c76d016584b524b522201f575a66bc0
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-11 16:36:43 -0200

    Add files via upload

--- gApi.js
+++ gApi.js
@@ -6,13 +6,18 @@ function onSignIn(googleUser) {
 	window.localStorage.setItem('image',  profile.getImageUrl());
 	window.location.href = 'Dashboard.html';
 
-   // $("#name").text(profile.getName());
-   // $("#email").text(profile.getEmail());
-  //  $("#image").attr('src', profile.getImageUrl());
+
 }
 
 
 
 
-
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+    });
+}
 
