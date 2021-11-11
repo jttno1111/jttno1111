6d59f12de792447e8cc47aff244f6231c55b4388
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-11 13:55:35 -0200

    Add files via upload

--- gApi.js
+++ gApi.js
@@ -15,7 +15,7 @@ function onSignIn(googleUser) {
     function renderButton() {
       gapi.signin2.render('my-signin2', {
         'scope': 'profile email',
-        'width': 100%,
+        'width': 240,
         'height': 50,
         'longtitle': true,
         'theme': 'dark',
