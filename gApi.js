a3271f697c4c5819ec40ec84a6ac27e7823cc8ff
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-11 15:55:58 -0200

    Add files via upload

--- gApi.js
+++ gApi.js
@@ -3,6 +3,11 @@ function onSignIn(googleUser) {
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $("#image").attr('src', profile.getImageUrl());
+	
+	
+	setTimeout(function(){
+    window.location.href = 'Dashboard.html';
+    }, 5000);
 }
 
 
