ecac579e87aec14a2418c36e668119241870c304
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-13 13:22:44 -0200

    Add files via upload

--- LMS/js/Admin_Dashboard.js
+++ LMS/js/Admin_Dashboard.js
@@ -12,14 +12,54 @@ function updateNarchive(){
 var primaryRightBox = document.getElementById("primary-right-box");
 var primaryBoxdisplay = getComputedStyle(primaryRightBox).display;
 var drpdwnbox = document.getElementById("actionToDrpDwn");
-var margin = getComputedStyle(drpdwnbox).margin;
+var UpDelBox = document.getElementById("upAndDelFormBox");
+var AccCreateBox = document.getElementById("accCreationBox");
+
 
 	if(primaryBoxdisplay == "none"){
 		primaryRightBox.style.display = "block";
 		drpdwnbox.style.right = "33%";
+		UpDelBox.style.display = "block";
+		AccCreateBox.style.display = "none";
+	}else{
+		UpDelBox.style.display = "block";
+		AccCreateBox.style.display = "none";
+		
 	}
+	
+	
 	drpdwnbox.style.display = "none";
 }
+
+
+
+function addLibMan(){
+var primaryRightBox = document.getElementById("primary-right-box");
+var primaryBoxdisplay = getComputedStyle(primaryRightBox).display;
+var drpdwnbox = document.getElementById("actionToDrpDwn");
+var UpDelBox = document.getElementById("upAndDelFormBox");
+var AccCreateBox = document.getElementById("accCreationBox");
+
+
+	if(primaryBoxdisplay == "none"){
+		primaryRightBox.style.display = "block";
+		drpdwnbox.style.right = "33%";
+		UpDelBox.style.display = "none";
+		AccCreateBox.style.display = "block";
+	}else{
+		UpDelBox.style.display = "none";
+		AccCreateBox.style.display = "block";
+		
+	}
+	
+	drpdwnbox.style.display = "none";
+}
+
+
+
+
+
+
 
 function closeRightBox(){
 var primaryRightBox = document.getElementById("primary-right-box");
