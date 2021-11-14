f1f42942c6f53eabb5e66aea5008dcafab119f6d
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-14 04:36:51 -0200

    Add files via upload

---
+++ rtbt/rtbt_js/gApi.js
@@ -0,0 +1,23 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+	window.localStorage.setItem('name', profile.getName());
+	window.localStorage.setItem('email', profile.getEmail());
+	window.localStorage.setItem('image',  profile.getImageUrl());
+	window.location.href = 'Dashboard.html';
+
+
+}
+
+
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+    });
+}
+
