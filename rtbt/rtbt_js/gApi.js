ed57b0b678c0a2f3d503393dcc654deff0dce015
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-14 08:47:47 -0200

    Add files via upload

--- rtbt/rtbt_js/gApi.js
+++ rtbt/rtbt_js/gApi.js
@@ -1,20 +1,25 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-
-	window.localStorage.setItem('name', profile.getName());
-	window.localStorage.setItem('email', profile.getEmail());
-	window.localStorage.setItem('image',  profile.getImageUrl());
-	window.location.href = 'Dashboard.html';
-
+    
+    $(".g-signin2").css("display", "none");
+	$(".LoginWrap").css("display", "none");
+	$(".DashboardWrap").css("display", "block");
+	
+	$(".data").css("display", "block");
+	$("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $(".image").attr('src', profile.getImageUrl());
 
 }
-
 
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+		$(".LoginWrap").css("display", "block");
+		$(".DashboardWrap").css("display", "none");
     });
 }
-
