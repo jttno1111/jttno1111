aa364c7c3b330380810287f787d4b25c2d0a45e3
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-14 06:10:12 -0200

    Add files via upload

---
+++ g-test/index.js
@@ -0,0 +1,15 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+	window.localStorage.setItem('name', profile.getName());
+	window.localStorage.setItem('email', profile.getEmail());
+	window.localStorage.setItem('image',  profile.getImageUrl());
+	window.location.href = 'landHere.html';
+	
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You have been signed out successfully");
+    });
+}
