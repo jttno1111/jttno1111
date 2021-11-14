ac68550f78a8d6f868dc77c8512e13346a87964a
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-14 06:35:36 -0200

    Update index.js

--- g-test/index.js
+++ g-test/index.js
@@ -3,13 +3,18 @@ function onSignIn(googleUser) {
 	window.localStorage.setItem('name', profile.getName());
 	window.localStorage.setItem('email', profile.getEmail());
 	window.localStorage.setItem('image',  profile.getImageUrl());
+	$(".data").css("display", "block");
+	$(".g-signin2").css("display", "none");
 	window.location.href = 'landHere.html';
+	
 	
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
+	$(".data").css("display", "none");
+	$(".g-signin2").css("display", "block");
         alert("You have been signed out successfully");
     });
 }
