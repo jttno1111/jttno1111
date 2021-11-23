99ae05156b4a424a868c8c7999172d3a435a7d3c
jttno1111 <93981703+jttno1111@users.noreply.github.com>
2021-11-23 16:06:18 -0200

    Add files via upload

--- rtbt/rtbt_js/gApi.js
+++ rtbt/rtbt_js/gApi.js
@@ -1,15 +1,6 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     
-    $(".g-signin2").css("display", "none");
-	$(".logReg").css("display", "none");
-	$(".abandoned").css("display", "block");
-	
-	$(".data").css("display", "block");
-	$("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-
 
 }
 
